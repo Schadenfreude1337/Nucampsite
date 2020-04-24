b20d02385438aa499bfb9d0dabb5284fab4d5caa
Nick <nmcnutt5@gmail.com>
2020-04-24 15:54:00 -0700

    first commit

---
+++ src/components/CampsiteInfoComponent.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+
+class CampsiteInfo extends Component {
+    renderCampsite(campsite) {
+        return (
+            <div className="col-md-5 m-1" >
+                <Card>
+                    <CardImg top src={campsite.image} alt={campsite.name} />
+                    <CardBody>
+                        <CardTitle>{campsite.name}</CardTitle>
+                        <CardText>{campsite.description}</CardText>
+                    </CardBody>
+                </Card>
+            </div>
+        );
+    }
+
+
+    render(campsite)
+     {
+        if (campsite) {
+            return (
+                <div className="row" >
+                    {this.renderCampsite(campsite)}
+                </div>
+            );
+        }
+        
+        else {
+        return (<div> </div>);
+        }
+    }
+}
+
+
+export default CampsiteInfo;
